808373363ea0900e6a42cc03a5e5914aa94ad9a4
Dave Boyce <daveb@501commons.org>
2019-06-25 08:41:37 -0700

    updates

--- .sfdx/typings/lwc/staticresources.d.ts
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -1,31 +1,3 @@
-declare module "@salesforce/resourceUrl/Bootstrap" {
-    var Bootstrap: string;
-    export default Bootstrap;
-}
-declare module "@salesforce/resourceUrl/Bootstrap4Force" {
-    var Bootstrap4Force: string;
-    export default Bootstrap4Force;
-}
-declare module "@salesforce/resourceUrl/FullCalendar" {
-    var FullCalendar: string;
-    export default FullCalendar;
-}
-declare module "@salesforce/resourceUrl/c501_sfdo213" {
-    var c501_sfdo213: string;
-    export default c501_sfdo213;
-}
-declare module "@salesforce/resourceUrl/c501_v4sStaticResources" {
-    var c501_v4sStaticResources: string;
-    export default c501_v4sStaticResources;
-}
-declare module "@salesforce/resourceUrl/c501_wtooltip" {
-    var c501_wtooltip: string;
-    export default c501_wtooltip;
-}
-declare module "@salesforce/resourceUrl/jQueryFiles" {
-    var jQueryFiles: string;
-    export default jQueryFiles;
-}
 declare module "@salesforce/resourceUrl/sfdo213" {
     var sfdo213: string;
     export default sfdo213;
@@ -33,8 +5,4 @@ declare module "@salesforce/resourceUrl/sfdo213" {
 declare module "@salesforce/resourceUrl/v4sStaticResources" {
     var v4sStaticResources: string;
     export default v4sStaticResources;
-}
-declare module "@salesforce/resourceUrl/wtooltip" {
-    var wtooltip: string;
-    export default wtooltip;
 }
